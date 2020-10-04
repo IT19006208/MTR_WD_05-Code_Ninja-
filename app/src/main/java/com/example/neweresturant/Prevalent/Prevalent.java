f0b63112c5644e6c0f0709a86bf627af309bc9b1
PASINDU <pasindudilhan111@gmail.com>
2020-10-04 23:52:35 +0530

    Cart

---
+++ app/src/main/java/com/example/neweresturant/Prevalent/Prevalent.java
@@ -0,0 +1,9 @@
+package com.example.neweresturant.Prevalent;
+
+public class Prevalent {
+   public  static Users currentOnlineUser;
+
+    private static final String UserPhoneKey="UserPhone";
+    private static final String UserPasswordKey="UserPassword";
+
+}
